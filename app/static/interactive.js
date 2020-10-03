96f4add75c1a1537780ec2b3b55a2725b98cb902
Gaurav <allmanpride@gmail.com>
2020-10-03 22:28:59 +0545

    made responsive design with uikit

--- app/static/interactive.js
+++ app/static/interactive.js
@@ -1,9 +1,9 @@
 function fill_sample() {
-    document.getElementById('kabita-maintext').value = "# शार्दुलविक्रीडित-छन्द-कविता\n# SSSIISISIIISSSISSIS \n# यसरि '#' पछाडि लेखिऐका लाईन जाच हुदैन।\n\nहोस् दीर्घायु सुस्वास्थ्य झै मधुरता वाणी मिठो नम्र होस्\nआफ्नो धर्म र कर्म मानमनिता भाषा कला चाख होस्\n\nइच्छा आँट खुशी सबै सफलहोस् तिम्रा सबै चाहना\nयो जन्मोत्सवले सधै खुश भरोस् हाम्रो छ यै कामना\n";
+    document.getElementById('kabita-maintext').value = "# शार्दुलविक्रीडित-छन्द-कविता\n# SSSIISISIIISSSISSIS \n# यसरि '#' पछाडि लेखिएका लाईन जाच हुदैन।\n\nहोस् दीर्घायु सुस्वास्थ्य झै मधुरता वाणी मिठो नम्र होस्\nआफ्नो धर्म र कर्म मानमनिता भाषा कला चाख होस्\n\nइच्छा आँट खुशी सबै सफलहोस् तिम्रा सबै चाहना\nयो जन्मोत्सवले सधै खुश भरोस् हाम्रो छ यै कामना\n";
 }
 
 function check_kabita() {
-    document.getElementById('kabita-response').innerHTML = "Analyzing...."
+    document.getElementById('kabita-response').innerHTML = " Analyzing...."
     data = {text : document.getElementById('kabita-maintext').value}
     return fetch("/check", {
 	method: "POST",
